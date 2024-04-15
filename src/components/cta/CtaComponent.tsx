de99fa09bc35288b9ae8473e3e592bd91bff63fb
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2024-04-15 22:50:53 +0200

    fix: cta component exports

--- src/components/cta/CtaComponent.tsx
+++ src/components/cta/CtaComponent.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { HTMLAttributes, createContext, forwardRef, useContext } from "react";
 import { CtaProps } from "./CtaProps";
 import "./cta.scss";
 import { Storytelling } from "@kickstartds/content/lib/storytelling";
@@ -6,65 +6,85 @@ import { ButtonContext } from "@kickstartds/base/lib/button";
 import classnames from "classnames";
 import { useButtonGroup } from "../button-group/ButtonGroupComponent";
 
-export const Cta: FC<CtaProps> = ({
-  headline,
-  highlightText = false,
-  sub,
-  image,
-  text,
-  textAlign,
-  backgroundImage,
-  backgroundColor,
-  colorNeutral,
-  contentAlign,
-  order,
-  fullWidth = false,
-  buttons = [],
-}) => {
-  const ButtonGroup = useButtonGroup();
+export const CtaContextDefault = forwardRef<
+  HTMLDivElement,
+  CtaProps & HTMLAttributes<HTMLDivElement>
+>(
+  (
+    {
+      headline,
+      highlightText = false,
+      sub,
+      image,
+      text,
+      textAlign,
+      backgroundImage,
+      backgroundColor,
+      colorNeutral,
+      contentAlign,
+      order,
+      fullWidth = false,
+      buttons = [],
+      ...rest
+    },
+    ref
+  ) => {
+    const ButtonGroup = useButtonGroup();
 
-  return (
-    <ButtonContext.Provider
-      // @ts-expect-error
-      value={ButtonGroup}
-    >
-      <Storytelling
-        className={classnames(
-          "dsa-cta",
-          fullWidth ? `dsa-cta--full-width` : "",
-          highlightText ? `dsa-cta--highlight-text` : "",
-          colorNeutral ? `dsa-cta--color-neutral` : "",
-          contentAlign && contentAlign !== "center"
-            ? `dsa-cta--align-${contentAlign}`
-            : ""
-        )}
-        backgroundImage={backgroundImage}
-        backgroundColor={backgroundColor}
-        full={image?.padding === false}
-        image={{
-          source: image?.src,
-          order: order,
-        }}
-        box={{
-          text: text,
-          textAlign: textAlign,
-          link: {
-            // @ts-expect-error
-            buttons,
-            colorNeutral: colorNeutral,
-            arrangement: textAlign,
-          },
-          headline: {
-            text: headline,
-            level: "h2",
-            style: highlightText === true ? "h1" : undefined,
-            sub: sub,
-            spaceAfter: highlightText === true ? "large" : undefined,
-            align: textAlign,
-          },
-        }}
-      />
-    </ButtonContext.Provider>
-  );
-};
+    return (
+      <ButtonContext.Provider
+        // @ts-expect-error
+        value={ButtonGroup}
+      >
+        <Storytelling
+          {...rest}
+          ref={ref}
+          className={classnames(
+            "dsa-cta",
+            fullWidth ? `dsa-cta--full-width` : "",
+            highlightText ? `dsa-cta--highlight-text` : "",
+            colorNeutral ? `dsa-cta--color-neutral` : "",
+            contentAlign && contentAlign !== "center"
+              ? `dsa-cta--align-${contentAlign}`
+              : ""
+          )}
+          backgroundImage={backgroundImage}
+          backgroundColor={backgroundColor}
+          full={image?.padding === false}
+          image={{
+            source: image?.src,
+            order: order,
+          }}
+          box={{
+            text: text,
+            textAlign: textAlign,
+            link: {
+              // @ts-expect-error
+              buttons,
+              colorNeutral: colorNeutral,
+              arrangement: textAlign,
+            },
+            headline: {
+              text: headline,
+              level: "h2",
+              style: highlightText === true ? "h1" : undefined,
+              sub: sub,
+              spaceAfter: highlightText === true ? "large" : undefined,
+              align: textAlign,
+            },
+          }}
+        />
+      </ButtonContext.Provider>
+    );
+  }
+);
+
+export const CtaContext = createContext(CtaContextDefault);
+export const Cta = forwardRef<
+  HTMLDivElement,
+  CtaProps & HTMLAttributes<HTMLDivElement>
+>((props, ref) => {
+  const Component = useContext(CtaContext);
+  return <Component {...props} ref={ref} />;
+});
 Cta.displayName = "Cta";
