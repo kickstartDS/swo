62afada2c982556ab00bc0e7bd28dc825643a252
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2024-10-24 13:55:54 +0200

    feat: execute scripts in html component

--- src/components/html/HtmlComponent.tsx
+++ src/components/html/HtmlComponent.tsx
@@ -1,18 +1,59 @@
-import { createContext, forwardRef, HTMLAttributes, useContext } from "react";
+import {
+  createContext,
+  forwardRef,
+  HTMLAttributes,
+  useContext,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from "react";
 import classNames from "classnames";
 import { HtmlProps } from "./HtmlProps";
 
 export type { HtmlProps };
 
+const copyScriptTag = (original: Element) => {
+  if (original.tagName !== "SCRIPT") {
+    return original;
+  }
+
+  const copy = document.createElement("script");
+  for (const attr of original.attributes) {
+    copy.setAttribute(attr.name, attr.value);
+  }
+  copy.textContent = original.textContent;
+
+  return copy;
+};
+
 export const HtmlContextDefault = forwardRef<
   HTMLDivElement,
   HtmlProps & HTMLAttributes<HTMLDivElement>
->(({ html, className, component, ...props }, ref) => {
+>(({ html, className, component, ...props }, forwardedRef) => {
+  const ref = useRef<HTMLDivElement>(null);
+  useImperativeHandle(forwardedRef, () => ref.current);
+
+  useEffect(() => {
+    if (html && ref.current) {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, "text/html");
+      const elements = [...doc.head.children, ...doc.body.children];
+      const insertedElements = elements.map((element) =>
+        ref.current.appendChild(copyScriptTag(element))
+      );
+
+      return () => {
+        for (const insertedElement of insertedElements) {
+          insertedElement?.remove();
+        }
+      };
+    }
+  }, [html, ref.current]);
+
   return (
     <div
       ref={ref}
       className={classNames("c-html", className)}
-      dangerouslySetInnerHTML={{ __html: html }}
       ks-component={component}
       {...props}
     />
